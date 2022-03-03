492f5e64bbcbe62dd32667d2ae64c43444b7d131
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-03 19:54:58 +0530

    Length of the Line

--- Line Comparison/src/com/bridgelabz/LineComparison.java
+++ Line Comparison/src/com/bridgelabz/LineComparison.java
@@ -1,9 +1,29 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class LineComparison {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison");
+		System.out.println("Line Comparison Program");	
+		
+		Scanner obj = new Scanner(System.in);
+		
+	  System.out.println("Enter the value of x1:");
+		int x1 = obj.nextInt();
+		
+		System.out.println("Enter the value of y1:");
+		int y1 = obj.nextInt();
+		
+		System.out.println("Enter the value of x2:");
+		int x2 = obj.nextInt();
+		
+		System.out.println("Enter the value of y2:");
+		int y2 = obj.nextInt();
+		
+		obj.close();
+		
+		double length= Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		
+		System.out.println("Distance Between two points is: " +length);
 	}
 
 }
